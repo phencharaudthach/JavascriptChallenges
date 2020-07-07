237e6955790f40fab3fd628baafa472dc0e73ab1
phencharaudthach <53065868+phencharaudthach@users.noreply.github.com>
2020-07-07 18:41:04 -0400

    working on hankerrank challenges

---
+++ hankerrank10days/day02_Switch.js
@@ -0,0 +1,14 @@
+
+function getLetter( s ){
+var setA = ["a,e,i,o,u"];
+var setB = ["b,c,d,f,g"];
+var setC = ["h,j,k,l,m"];
+var setD = ["n,p,q,r,s,t,v,w,x,y,z"];
+
+var letter = s.slice(0,1);
+
+
+for (var i = 0; i < )
+}
+
+console.log(getLetter("adfgt"));
