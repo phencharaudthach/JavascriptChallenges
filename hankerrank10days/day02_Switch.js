b43ca587bec27b91e0145ebd233a5930f1d466b7
phencharaudthach <53065868+phencharaudthach@users.noreply.github.com>
2020-07-13 17:54:29 -0400

    done with switch conditional statements

--- hankerrank10days/day02_Switch.js
+++ hankerrank10days/day02_Switch.js
@@ -1,14 +1,36 @@
+function getLetter(s) {
+  var setA = ['a,e,i,o,u'];
+  var setB = ['b,c,d,f,g'];
+  var setC = ['h,j,k,l,m'];
+  var setD = ['n,p,q,r,s,t,v,w,x,y,z'];
 
-function getLetter( s ){
-var setA = ["a,e,i,o,u"];
-var setB = ["b,c,d,f,g"];
-var setC = ["h,j,k,l,m"];
-var setD = ["n,p,q,r,s,t,v,w,x,y,z"];
+  var letter = s.slice(0, 1);
 
-var letter = s.slice(0,1);
-
-
-for (var i = 0; i < )
+  switch (letter) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+      return 'A';
+      break;
+    case 'b':
+    case 'c':
+    case 'd':
+    case 'f':
+    case 'g':
+      return 'B';
+      break;
+    case 'h':
+    case 'j':
+    case 'k':
+    case 'l':
+    case 'm':
+      return 'C';
+      break;
+    default:
+      return 'D';
+  }
 }
 
-console.log(getLetter("adfgt"));
+console.log(getLetter('dfgt'));
