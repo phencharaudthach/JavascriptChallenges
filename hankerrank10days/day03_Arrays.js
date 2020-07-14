504931a5fe5a2f05a6ab06c10772b81c5712b364
phencharaudthach <53065868+phencharaudthach@users.noreply.github.com>
2020-07-14 17:16:07 -0400

    day 03 arrays

---
+++ hankerrank10days/day03_Arrays.js
@@ -0,0 +1,12 @@
+function getSecondLargestNums(nums) {
+  var seondLarge = 0;
+  for (var i = 0; i < nums.length - 1; i++) {
+    // console.log(nums[i]);
+    if (nums[i] > nums[i + 1]) {
+      var largestNum = nums[i];
+      console.log(largestNum);
+      console.log(nums.indexOf(nums[i]));
+    }
+  }
+}
+console.log(getSecondLargestNums([2, 3, 6, 6, 5]));
